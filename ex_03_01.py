fdd8b742a42d137498e885189716e4fe9ab79d5a
Baldwin24777 <156390591+Baldwin24777@users.noreply.github.com>
2024-02-02 22:33:41 -0800

    Add files via upload

---
+++ ex_03_01.py
@@ -0,0 +1,24 @@
+
+sh = input("Enter Hours: ")
+sr = input("Enter Rate: ")
+fh = float(sh)
+fr = float(sr)
+# print(fh, fr)
+if  fh > 40:
+    # print("Overtime")
+    reg = fh * fr
+    otp = (fh - 40) * (0.5 * fr)
+    # print(reg, otp)
+    xp = reg + otp
+    print(xp)
+else:
+    # print("Regular")
+    xp = fh * fr
+print("Pay:",xp)
+
+
+
+
+
+
+
